69aebbeea44c4d9ba4a59fe7bafe93a889aa205d
Tisk_01010100 <155505304+systemcmd0122@users.noreply.github.com>
2024-06-28 10:21:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -66,6 +66,21 @@ for (const command of commands) {
 // クライアントが準備できたときのイベントリスナー
 client.once(Events.ClientReady, c => {
     console.log(`準備OKです! ${c.user.tag}がログインします。`);
+
+    // 定期的にアクティビティを発生させるためのコード
+    const channelId = 1256055208006389884; // メッセージを送信するチャンネルID
+    const interval = 15 * 60 * 1000; // 15分間隔
+
+    setInterval(() => {
+        const channel = client.channels.cache.get(channelId);
+        if (channel) {
+            channel.send('定期的なメッセージです。Renderのスリープを防ぎます。')
+                .then(() => console.log('定期メッセージを送信しました。'))
+                .catch(console.error);
+        } else {
+            console.error('チャンネルが見つかりません。');
+        }
+    }, interval);
 });
 
 // interactionCreateイベントのリスナー設定
